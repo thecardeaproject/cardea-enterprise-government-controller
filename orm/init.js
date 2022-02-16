0841091f361b3b35f96d70500a26e0f75664c1dc
Kim Ebert <kim@indicio.tech>
2022-02-16 10:49:58 -0700

    feature: update docker configuration
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- orm/init.js
+++ orm/init.js
@@ -1,8 +1,8 @@
 const {Sequelize} = require('sequelize')
 
 connect = function () {
-  const sequelize = new Sequelize('government', 'government', 'government', {
-    host: 'government-db',
+  const sequelize = new Sequelize(process.env.DB, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false, //console.log, // Log to console or false (no logging of database queries)
     omitNull: true,
